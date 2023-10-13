6ad381427de7f1b739bcea4492181eebb8066e13
Aydar <145969068+Aydarchik02@users.noreply.github.com>
2023-10-13 08:01:23 +0300

    Update Main.java
    ChoTam

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Aydarchik007");
+        System.out.println("AydarLast");
     }
 }
