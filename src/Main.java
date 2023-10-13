ed00d3fb73b1739c82a43c7f60c9a0a6c5adcd38
Aydarchik01 <Aydarzhaparov@gmail.com>
2023-10-13 11:12:09 +0300

    Aydarchik007

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,7 @@ public class Main {
         System.out.println("Cho taaam");
         System.out.println("Cho taaam");
         System.out.println("Cho taaam");
-
+        Animal animal = new Animal();
 
     }
 }
