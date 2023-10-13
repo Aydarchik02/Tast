3cd5c798e3c2fa5b51d6df8a30d0e62b20d62e8e
Aydarchik01 <Aydarzhaparov@gmail.com>
2023-10-13 14:48:53 +0300

    dar

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,72 @@
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Aydarchik007");
-        System.out.println("AydarLast");
-                System.out.println("AydarLast");
-        System.out.println("AydarLast");
-        System.out.println("AydarLast");
-        System.out.println("AydarLast");
+//        String[] str = {"Ayadr", "FGH", "fvghjkl"};
+//        Stream.of(str).map(x -> x + " Kot ").map(x -> x + 4).forEach(System.out::println);
+//        List<String> list = Arrays.asList("hj", "ghjk", "dedw");
+//        list.forEach(System.out::println);
+//
+//
+//
+//        int [] integerArrays = {12,4,2,234};
+//        Arrays.stream(integerArrays).map(x -> x*x).forEach(System.out::println);
 
 
-        System.out.println("Cho taaam");
+//        Random random = new Random();
+//        List<Integer> intlist = new ArrayList<>();
+//        for (int i = 0; i < 20; i++) {
+//            int a = random.nextInt(20, 40);
+//            intlist.add(a);
+//        }
+//      List<Integer> eventNumbers = intlist.stream().filter( n -> n % 2==0).map(n -> n+2 ).toList();
+//        System.out.println(eventNumbers);
 
-                System.out.println("Cho taaam");
-        System.out.println("Cho taaam");
-        System.out.println("Cho taaam");
-        System.out.println("Cho taaam");
-        System.out.println("Cho taaam");
-        Animal animal = new Animal();
-        animal.name = "Cat";
-        animal.age = 5;
-        System.out.println(animal);
-        Animal animal1 = new Animal();
-        animal1.name= "fgh";
-        animal1.age = 67;
-        System.out.println(animal1);
+
+//        List<Object> words = Arrays.asList(23, 123.3, "cherry");
+//        AtomicInteger i = new AtomicInteger();
+//        List<Object> wordLengths = words.stream()
+//                .map(n -> " Element" + i.getAndIncrement() + ": " + n)
+//                .collect(Collectors.toList());
+//        System.out.println(wordLengths);
+//
+
+
+
+
+//        List<String> numbers = Arrays.asList("asd","asfdsadf","asfd");
+//        List<String> sortedNumbers = numbers.stream()
+//                .sorted()
+//                .collect(Collectors.toList());
+//        System.out.println(sortedNumbers);
+
+
+
+//        List<String> colors = Arrays.asList("red", "green", "blue", "red", "yellow");
+//        List<String> uniqueColors = colors.stream()
+//                .distinct()
+//                .collect(Collectors.toList());
+//
+//        System.out.println(uniqueColors);
+//
+
+//        List<Integer> numbers = Arrays.asList(1,12,34,1,24,234,235,12);
+//        List<Integer> limitedNumbers = numbers.stream()
+//                .limit(3)
+//                .collect(Collectors.toList());
+
+
+//
+//        System.out.println(limitedNumbers);
+//
+//        List<Integer> skippedNumbers = numbers.stream()
+//                .skip(1)
+//                .collect(Collectors.toList());
+//
+//        System.out.println(skippedNumbers);
+
     }
 }
